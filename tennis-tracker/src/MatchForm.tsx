866740b38da03da9f3f02e99ffeacd2e12da7e4b
lwang79 <lucy.wang7@gmail.com>
2026-06-22 23:25:37 +1000

    update matchform

--- tennis-tracker/src/MatchForm.tsx
+++ tennis-tracker/src/MatchForm.tsx
@@ -1,51 +1,76 @@
 import { useState } from "react";
-import type { Match } from '../src/types'
+import type { Match } from './types'
 
-// define props
+// define props - this is what the component receives from the app
 interface MatchFormProps {
-  onSubmit: (match: Match) => void;
+  submitForm: (match: Match) => void;
 }
 
 // define fields required
 interface FormFields {
+  date: string;
   playerOne: string;
   playerTwo: string;
-  date: string;
-  playerOneScore: string;
-  playerTwoScore: string;
+  scoreOne: string;
+  scoreTwo: string;
 
 }
 
 // FormFields empty state
-const empty: FormFields = {
+const form: FormFields = {
   date: '',
   playerOne: '',
   playerTwo: '',
-  playerOneScore: '',
-  playerTwoScore: '',
+  scoreOne: '',
+  scoreTwo: '',
 }
 
 
 // form component to be wired up to App.tsx
-export default function MatchForm({ onSubmit }: MatchFormProps) {
+export default function MatchForm({ submitForm }: MatchFormProps) {
 
   // state that needs to be tracked
-  const [fields, setFields] = useState<FormFields>(empty)
+  const [fields, setFields] = useState<FormFields>(form)
 
   // onchange handler that fires on every keystroke
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    
+    setFields(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }))
   }
 
   // submit handler - to fire when form is submited
   function handleSubmit(e: React.SubmitEvent) {
-    e.preventDefault()
+    e.preventDefault();
+    // do nothing if fields have no values
+    if (Object.values(fields).some(value => value === '')) return;
+    // create new match if fields are populated
+    const newMatch: Match = {
+      id: Date.now().toString(),
+      date: fields.date,
+      playerOne: fields.playerOne,
+      playerTwo: fields.playerTwo,
+      scoreOne: parseInt(fields.scoreOne),
+      scoreTwo: parseInt(fields.scoreTwo)
+    }
+    submitForm(newMatch);
+    setFields(form);
+
+
   }
 
   return (
+    <div>
+    <input type="text" name="date" value={fields.date} onChange={handleChange}/>
+    <input type="text" name="playerOne" value={fields.playerOne} onChange={handleChange}/>
+    <input type="text" name="playerTwo" value={fields.playerTwo} onChange={handleChange}/>
+    <input type="text" name="scoreOne" value={fields.scoreOne} onChange={handleChange}/>
+    <input type="text" name="scoreTwo" value={fields.scoreTwo} onChange={handleChange}/>
     <form onSubmit={handleSubmit}>
 
     </form>
+    </div>
   )
 
 
